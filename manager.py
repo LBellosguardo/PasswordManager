36716c4222990ff68a725d48f7c71bbca993f907
Louis Bellosguardo <louis.bellosguardo@gmail.com>
2020-10-05 15:33:51 -0700

    Moved db interactions to their own methods
    Add password moved to a function
    
    Delete password moved to a function
    
    Retrieve password moved to a function

--- manager.py
+++ manager.py
@@ -37,6 +37,75 @@ def create_connection(db_file):
         print(e)
     return conn
 
+def retrieve_password(conn, cursor):
+    
+    print('Services:')
+    cursor.execute('SELECT service FROM passwords ORDER BY service')
+    print(cursor.fetchall())
+    service = input('Which service would you like to retrieve information for?\n').lower()
+    
+    cursor.execute('SELECT * FROM passwords WHERE service == ?', (service,))
+    result = cursor.fetchone()
+    if result != None:    
+        user = dbe.decrypt(service, result[1])
+        password = dbe.decrypt(service, result[2])
+        print(f'Username: {user}')
+        print(f'Password: {password}')
+        choice = input("Press 'm' to return to the main menu\n").lower()
+        while choice != 'm':
+            choice = input("Press 'm' to return to the main menu\n").lower()
+                
+    else:
+        print(f'No entry for {service} was found')
+        sleep(3)
+
+def add_password(conn, cursor):
+    
+    service = input('What is the name of the website or service?\n')
+    u_name = input(f'What is your {service} username? (This can also be your email)\n')
+    choice = input('Would you like a password to be automatically generated for you? (y/n)\n')
+    if choice == 'y':
+        pw = generate_password()
+        cursor.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, dbe.encrypt(service, u_name), dbe.encrypt(service, pw)))
+        print(f'Your newly generated password is: {pw}\nIt has been encrypted and saved to your vault.')
+    else:
+        pw, pw2 = 'abc', 'xyz'
+        while pw != pw2:
+            # Have user enter password twice to ensure their entry is correct
+            pw = getpass('Please enter your desired password:\n')
+            pw2 = getpass('Please confirm password:\n')
+            if pw == 'm' or pw2 == 'm':
+                break
+            if pw != pw2:
+                print('Passwords did not match, please try again, or press m for main menu')
+        if pw == pw2:
+            cursor.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, dbe.encrypt(service, u_name), dbe.encrypt(service, pw)))
+    conn.commit()
+    
+def delete_password(conn, cursor):
+        print('Services:')
+        cursor.execute('SELECT service FROM passwords ORDER BY service')
+        print(cursor.fetchall())
+        service = input('Which service would you like to delete the information for?\n').lower()
+        
+        cursor.execute('SELECT * FROM passwords WHERE service == ?', (service,))
+        result = cursor.fetchone()
+        if result != None:
+            confirm = getpass(f"Are you sure you want to delete the entry for {service}? " 
+                                + "This cannot be undone.\nEnter your master password to proceed\n")
+            if confirm == MASTER:
+                cursor.execute('DELETE FROM passwords WHERE service = ?', (service,))
+                print(f'Entry for {service} successfully deleted. Returning to main menu...')
+                sleep(3)
+            else:
+                print('Master password does not match. Returning to main menu...')
+                sleep(3)
+        else:
+            print(f"No entry for {service} was found. Returning to main menu...")
+            sleep(3)
+        
+        conn.commit()
+    
 if connect == MASTER:
     
     conn = create_connection('password_manager.db')
@@ -54,7 +123,7 @@ if connect == MASTER:
         print('Main Menu')
         print('r: Retrieve one of your usernames and password')
         print('a: Add a new username and password')
-        print("u: Update an existing entry (username, password, or both")
+        print("u: Update an existing entry (username, password, or both)")
         print('d: Delete the information for a particular service')
         print("g: Generate a new secure password (does not save this password for now)")
         print("c: Change your master password (maybe, idk if this feature will stay)")
@@ -69,69 +138,13 @@ if connect == MASTER:
             sys.exit()
 
         if command == 'a':
-            service = input('What is the name of the website or service?\n')
-            u_name = input(f'What is your {service} username? (This can also be your email)\n')
-            choice = input('Would you like a password to be automatically generated for you? (y/n)\n')
-            if choice == 'y':
-                pw = generate_password()
-                c.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, dbe.encrypt(service, u_name), dbe.encrypt(service, pw)))
-                print(f'Your newly generated password is: {pw}\nIt has been encrypted and saved to your vault.')
-            else:
-                pw, pw2 = 'abc', 'xyz'
-                while pw != pw2:
-                    # Have user enter password twice to ensure their entry is correct
-                    pw = getpass('Please enter your desired password:\n')
-                    pw2 = getpass('Please confirm password:\n')
-                    if pw == 'm' or pw2 == 'm':
-                        break
-                    if pw != pw2:
-                        print('Passwords did not match, please try again, or press m for main menu')
-                if pw == pw2:
-                    c.execute("INSERT INTO passwords VALUES (?, ?, ?)", (service, dbe.encrypt(service, u_name), dbe.encrypt(service, pw)))
-            conn.commit()
-        
-        if command == 'r':
-            print('Services:')
-            c.execute('SELECT service FROM passwords ORDER BY service')
-            print(c.fetchall())
-            service = input('Which service would you like to retrieve information for?\n').lower()
+            add_password(conn, c)
             
-            c.execute('SELECT * FROM passwords WHERE service == ?', (service,))
-            result = c.fetchone()
-            if result != None:    
-                user = dbe.decrypt(service, result[1])
-                password = dbe.decrypt(service, result[2])
-                print(f'Username: {user}')
-                print(f'Password: {password}')
-                choice = input("Press 'm' to return to the main menu\n").lower()
-                while choice != 'm':
-                    choice = input("Press 'm' to return to the main menu\n").lower()
-                        
-            else:
-                print(f'No entry for {service} was found')
-                sleep(3)
+        if command == 'r':
+            retrieve_password(conn, c)
         
         if command == 'd':
-            print('Services:')
-            c.execute('SELECT service FROM passwords ORDER BY service')
-            print(c.fetchall())
-            service = input('Which service would you like to delete the information for?\n').lower()
-            
-            c.execute('SELECT * FROM passwords WHERE service == ?', (service,))
-            result = c.fetchone()
-            if result != None:
-                confirm = getpass(f"Are you sure you want to delete the entry for {service}?" 
-                                 + "This cannot be undone.\n Enter your master password to proceed\n")
-                if confirm == MASTER:
-                    c.execute('DELETE FROM passwords WHERE service = ?', (service,))
-                    print(f'Entry for {service} successfully deleted. Returning to main menu...')
-                    sleep(3)
-                else:
-                    print('Master password does not match. Returning to main menu...')
-                    sleep(3)
-            else:
-                print(f"No entry for {service} was found. Returning to main menu...")
-                sleep(3)
-                    
+            delete_password(conn, c)      
+              
         if command == 'g':
             print(generate_password())

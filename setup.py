8b512e6f1c8acdb75cbd2e7b3205c6fbe761aff2
Louis Bellosguardo <louis.bellosguardo@gmail.com>
2020-10-11 13:40:45 -0400

    Added setup.py

--- setup.py
+++ setup.py
@@ -3,17 +3,21 @@ from pathlib import Path
 import sys
 from getpass import getpass
 
-master = getpass('Enter a master password:\n')
-master2 = getpass('Confirm master password:\n')
+if 'PM_MASTER' not in os.environ:
+    master = getpass('Enter a master password:\n')
+    master2 = getpass('Confirm master password:\n')
 
-if master == master2:
-    if 'zsh' in os.environ.get("SHELL", ""):
-        with open(Path.home() / ".zshrc", 'a') as f:
-            f.write(f'\nexport PM_MASTER="{master}"')
-        f.close()
-    # os.environ['PM_MASTER'] = str(master)
+    if master == master2:
+        if 'zsh' in os.environ.get("SHELL", ""):
+            with open(Path.home() / ".zshrc", 'a') as f:
+                f.write(f'\nexport PM_MASTER="{master}"')
+            f.close()
+    else:
+        print('Passwords did not match. Ending program...')
+        sys.exit()
+
+    print(os.urandom(16))
+    print('Copy the above byte string into the empty salt variable in dbencrypt.py')
+
 else:
-    sys.exit()
-
-print(os.urandom(16))
-print('Copy the above byte string into the empty salt variable in dbencrypt.py')
+    print('Password manager has already been set up')
